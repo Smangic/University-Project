cee824e86922dd8d2ce87adba29ab0d258239ccc
Smangic <83532013+Smangic@users.noreply.github.com>
2021-05-16 15:04:12 +0800

    Update GridComponent.java
    year

--- GridComponent.java
+++ GridComponent.java
@@ -4,36 +4,43 @@ import entity.GridStatus;
 import minesweeper.MainFrame;
 
 import java.awt.*;//换swing
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.Serializable;
 import javax.swing.*;
 import javax.swing.text.IconView;
 
-public class GridComponent extends BasicComponent {
+public class GridComponent extends BasicComponent implements Serializable {
     public static int gridSize = 50;//调整大小需要改这个
 
 
-    private static final Image grid = Toolkit.getDefaultToolkit().getImage("src/Icon/Grass.png");
-    private static final Image Base = Toolkit.getDefaultToolkit().getImage("src/Icon/BaseGround.jpg");
-    private static final Image Mine = Toolkit.getDefaultToolkit().getImage("src/Icon/TNT.jpg");
-    private static final Image Flag = Toolkit.getDefaultToolkit().getImage("src/Icon/Flag.png");
-    private static final Image zero = Toolkit.getDefaultToolkit().getImage("src/Icon/0.png");
-    private static final Image one = Toolkit.getDefaultToolkit().getImage("src/Icon/1.png");
-    private static final Image two = Toolkit.getDefaultToolkit().getImage("src/Icon/2.png");
-    private static final Image three = Toolkit.getDefaultToolkit().getImage("src/Icon/3.png");
-    private static final Image four = Toolkit.getDefaultToolkit().getImage("src/Icon/4.png");
-    private static final Image five = Toolkit.getDefaultToolkit().getImage("src/Icon/5.png");
-    private static final Image six = Toolkit.getDefaultToolkit().getImage("src/Icon/6.png");
-    private static final Image seven = Toolkit.getDefaultToolkit().getImage("src/Icon/7.png");
-    private static final Image eight = Toolkit.getDefaultToolkit().getImage("src/Icon/8.png");
+    private static final Image grid = Toolkit.getDefaultToolkit().getImage("MineSweeper-Demo/src/Icon/Grass.png");
+    private static final Image Base = Toolkit.getDefaultToolkit().getImage("MineSweeper-Demo/src/Icon/BaseGround.jpg");
+    private static final Image Mine = Toolkit.getDefaultToolkit().getImage("MineSweeper-Demo/src/Icon/TNT.jpg");
+    private static final Image Flag = Toolkit.getDefaultToolkit().getImage("MineSweeper-Demo/src/Icon/Flag.png");
+    private static final Image zero = Toolkit.getDefaultToolkit().getImage("MineSweeper-Demo/src/Icon/0.png");
+    private static final Image one = Toolkit.getDefaultToolkit().getImage("MineSweeper-Demo/src/Icon/1.png");
+    private static final Image two = Toolkit.getDefaultToolkit().getImage("MineSweeper-Demo/src/Icon/2.png");
+    private static final Image three = Toolkit.getDefaultToolkit().getImage("MineSweeper-Demo/src/Icon/3.png");
+    private static final Image four = Toolkit.getDefaultToolkit().getImage("MineSweeper-Demo/src/Icon/4.png");
+    private static final Image five = Toolkit.getDefaultToolkit().getImage("MineSweeper-Demo/src/Icon/5.png");
+    private static final Image six = Toolkit.getDefaultToolkit().getImage("MineSweeper-Demo/src/Icon/6.png");
+    private static final Image seven = Toolkit.getDefaultToolkit().getImage("MineSweeper-Demo/src/Icon/7.png");
+    private static final Image eight = Toolkit.getDefaultToolkit().getImage("MineSweeper-Demo/src/Icon/8.png");
     private int row;
     private int col;
     private GridStatus status = GridStatus.Covered;//可以考虑于Square进行衔接
     private String content;
+    private MouseAdapter adapter;
 
     public GridComponent(int x, int y) {
         this.setSize(gridSize, gridSize);//调整大小需要改这个
         this.row = x;
         this.col = y;
+        this.addMouseListener(adapter);
+
     }
+
 
     @Override
     public void onMouseLeftClicked() {
@@ -44,21 +51,22 @@ public class GridComponent extends BasicComponent {
 
 
             if (content.equals("-1")) {
-                if (MainFrame.controller.getOnTurnPlayer() == MainFrame.controller.getP1()) {
-                    MainFrame.controller.getP1().costp1Score();
-                    MainFrame.controller.addNumberOfLandmineBoom();
-                } else if (MainFrame.controller.getOnTurnPlayer() == MainFrame.controller.getP2()) {
-                    MainFrame.controller.getP2().costp2Score();
-                    MainFrame.controller.addNumberOfLandmineBoom();
+                if (Starter.mainFrame.controller.getOnTurnPlayer() == Starter.mainFrame.controller.getP1()) {
+                    Starter.mainFrame.controller.getP1().costp1Score();
+                    Starter.mainFrame.controller.addNumberOfLandmineBoom();
+                    System.out.println("Yes");
+                } else if (Starter.mainFrame.controller.getOnTurnPlayer() == Starter.mainFrame.controller.getP2()) {
+                    Starter.mainFrame.controller.getP2().costp2Score();
+                    Starter.mainFrame.controller.addNumberOfLandmineBoom();
                 }
             }
-            MainFrame.controller.getScoreBoard().update();
+            Starter.mainFrame.controller.getScoreBoard().update();
 
 
-            MainFrame.controller.addClick();
-            if (MainFrame.controller.getClick() == MainFrame.controller.getTimes()) {
-                MainFrame.controller.nextTurn();
-                MainFrame.controller.setClick(0);
+            Starter.mainFrame.controller.addClick();
+            if (Starter.mainFrame.controller.getClick() == Starter.mainFrame.controller.getTimes()) {
+                Starter.mainFrame.controller.nextTurn();
+                Starter.mainFrame.controller.setClick(0);
 
             }
 
@@ -69,33 +77,33 @@ public class GridComponent extends BasicComponent {
 
     @Override
     public void onMouseRightClicked() {
-        System.out.printf("Gird (%d,%d) is right-clicked.\n", row, col);
+        System.out.printf("Gird (%d,%d) is right-clicked.\n", row ,col);
         if (this.status == GridStatus.Covered) {
             this.status = GridStatus.Flag;
             repaint();
-            MainFrame.controller.addClick();
+            Starter.mainFrame.controller.addClick();
 
             if (content.equals("-1") && this.status == GridStatus.Flag) {
-                if (MainFrame.controller.getOnTurnPlayer() == MainFrame.controller.getP1()) {
-                    MainFrame.controller.getP1().addp1Score();
-                    MainFrame.controller.addNumberOfLandmineFlag();
-                } else if (MainFrame.controller.getOnTurnPlayer() == MainFrame.controller.getP2()) {
-                    MainFrame.controller.getP2().addp2Score();
-                    MainFrame.controller.addNumberOfLandmineFlag();
+                if (Starter.mainFrame.controller.getOnTurnPlayer() == Starter.mainFrame.controller.getP1()) {
+                    Starter.mainFrame.controller.getP1().addp1Score();
+                    Starter.mainFrame.controller.addNumberOfLandmineFlag();
+                } else if (Starter.mainFrame.controller.getOnTurnPlayer() == Starter.mainFrame.controller.getP2()) {
+                    Starter.mainFrame.controller.getP2().addp2Score();
+                    Starter.mainFrame.controller.addNumberOfLandmineFlag();
                 }
             }
             if (!content.equals("-1") && this.status == GridStatus.Flag) {
-                if (MainFrame.controller.getOnTurnPlayer() == MainFrame.controller.getP1()) {
-                    MainFrame.controller.getP1().addp1mistake();
-                } else if (MainFrame.controller.getOnTurnPlayer() == MainFrame.controller.getP2()) {
-                    MainFrame.controller.getP2().addp2mistake();
+                if (Starter.mainFrame.controller.getOnTurnPlayer() == Starter.mainFrame.controller.getP1()) {
+                    Starter.mainFrame.controller.getP1().addp1mistake();
+                } else if (Starter.mainFrame.controller.getOnTurnPlayer() == Starter.mainFrame.controller.getP2()) {
+                    Starter.mainFrame.controller.getP2().addp2mistake();
                 }
             }
-            MainFrame.controller.getScoreBoard().update();
+            Starter.mainFrame.controller.getScoreBoard().update();
 
-            if (MainFrame.controller.getClick() == MainFrame.controller.getTimes()) {
-                MainFrame.controller.nextTurn();
-                MainFrame.controller.setClick(0);
+            if (Starter.mainFrame.controller.getClick() == Starter.mainFrame.controller.getTimes()) {
+                Starter.mainFrame.controller.nextTurn();
+                Starter.mainFrame.controller.setClick(0);
 
             }
         }
@@ -107,6 +115,7 @@ public class GridComponent extends BasicComponent {
 
         if (this.status == GridStatus.Covered) {
             g.drawImage(grid, 0, 0, getWidth() - 1, getHeight() - 1, this);
+            //if(this.mouseExited())
         }
         if (this.status == GridStatus.Clicked) {
             if (Integer.parseInt(content) == -1) {
@@ -163,5 +172,15 @@ public class GridComponent extends BasicComponent {
     public void paintComponent(Graphics g) {
         super.printComponents(g);
         draw(g);
+    }
+
+    @Override
+    public String toString() {
+        return "GridComponent{" +
+                "row=" + row +
+                ", col=" + col +
+                ", status=" + status +
+                ", content='" + content + '\'' +
+                '}';
     }
 }
